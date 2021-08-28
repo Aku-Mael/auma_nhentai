b80696c99ee5afbd94978f4dac988177bbe56199
Aku-Mael <unahojasuelta@gmail.com>
2021-08-28 11:09:53 -0700

    Now DownloadHentai inherits from Hentai and have properties methods

--- downloadNhentai.py
+++ downloadNhentai.py
@@ -5,7 +5,7 @@ from shutil import rmtree
 from PIL import Image
 from urllib.request import urlopen
 from io import BytesIO
-from nhentai import Hentai
+from nhentai import Hentai, ConnectionErrorNHentai
 
 
 HENTAI_PAGE = "https://nhentai.net/g/"
@@ -13,29 +13,42 @@ HENTAI_PICTURES = "https://i.nhentai.net/galleries/"
 COVER_PAGE = "https://t.nhentai.net/galleries/"
 
 
-class DownloadHentai():
+class DownloadHentai(Hentai):
 
-    def __init__(self, hentai):
+    def __init__(self, code):
 
+        super().__init__(code)
         # Hentai information
-        self.hentai = hentai
-        self.mainURL = self.hentai.title + '/'
+        self._directory = None
+        self._troubledPages = []
         
-    def setUrl(self, url):
-        self.mainURL = url + '/' + self.hentai.title + '/'
+    @property
+    def directory(self):
+        return self._directory
+
+    @property
+    def troubledPages(self):
+        return self._troubledPages
+
+    def setDirectory(self, url):
+        self._directory = url + '/' + self.title + '/'
 
     def downloadHentai(self):
+
+        if self._directory is None:
+            self._directory = self.title + '/'
+
         try:
-            rmtree(self.mainURL)
-        except:
+            rmtree(self._directory)
+            print('Delete folder "' + self._directory + '"')
+        except FileNotFoundError:
             pass
 
-        os.mkdir(self.mainURL)
-        pages = int(self.hentai.pages)
+        os.mkdir(self._directory)
+        pages = int(self.pages)
         for i in range(pages):
             num_page = str(i + 1)
-            self.create_image(HENTAI_PICTURES + self.hentai.id + '/' + num_page, num_page)
-            
+            self.create_image(HENTAI_PICTURES + self.id + '/' + num_page, num_page)
         return True
 
     def create_image(self, pre_url, name):
@@ -49,42 +62,46 @@ class DownloadHentai():
                 data = urlopen(url).read()
                 file = BytesIO(data)
                 img = Image.open(file)
-                
-                img.save(self.mainURL + name + ext)
+                img.save(self._directory + name + ext)
                 return
-                
 
-        print("Image extension problem")
+        self._troubledPages.append(name)
         return None
-    
-    def get_cover(self):
-        extensiones = [".jpg", ".png"]
-
-        for ext in extensiones:
-            url = COVER_PAGE + self.hentai.id + "/cover" + ext
-            print(url)
-            check = requests.get(url)
-
-            if check.status_code != 404:
-                data = urlopen(url).read()
-                file = BytesIO(data)
-                img = Image.open(file)
-                return img
 
     def __str__(self):
-        string = str(self.hentai) + '\n'
-        string = string + 'URL: ' + self.mainURL
+        string = "\nHentai info:\n" + str(super().__str__())
+        string = string + "\n\nDownload Properties:"
+        if self._directory:
+            string = string + '\nURL: ' + self._directory
+        if self._troubledPages:
+            string = string + '\nTrouble Pages ' + str(self._troubledPages)
         return string
+
+
+def downloadHentai(code):
+    try:
+        int(code)
+    except ValueError:
+        print("Write a code")
+        return
+
+    hentai = DownloadHentai(sys.argv[1])
+
+    try:
+        hentai.loadData()
+    except ConnectionErrorNHentai as e:
+        print(e)
+        return
+    hentai.analyzeData()
+    hentai.downloadHentai()
+    print("###############")
+    print("# Hentai Info #")
+    print("###############")
+    print(hentai)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(sys.argv[0] + " code")
-        exit()
+        print("Write a code")
     else:
-        temp = Hentai(sys.argv[1])
-        temp.loadData()
-        temp.analyzeData()
-        hentai = DownloadHentai(temp)
-        hentai.downloadHentai()
-
-        
+        downloadHentai(sys.argv[1])
